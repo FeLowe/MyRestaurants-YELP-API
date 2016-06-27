8bae93bc6292507c45791f477838846c7a528b18
Daniel Wulff and Nick Lyman <daniel@>
2016-06-27 14:25:10 -0700

    Pass local unit testing and instrumentation tests on emulator and Nexus 5.

--- app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
@@ -1,13 +1,19 @@
 package com.epicodus.myrestaurants;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Build;
+import android.widget.TextView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by Guest on 6/27/16.
@@ -22,5 +28,20 @@ public class MainActivityTest {
     @Before
     public void setup() {
         activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.findRestaurantsButton).performClick();
+        Intent expectedIntent = new Intent(activity, RestaurantsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
